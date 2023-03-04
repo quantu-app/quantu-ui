1f72254c327070ec31aafb8bbbba8a9ae1b09788
Nathan Faucett <nathanfaucett@gmail.com>
2023-03-04 13:04:41 -0500

    fix quiz link, again

--- src/lib/components/quiz/Quizzes.svelte
+++ src/lib/components/quiz/Quizzes.svelte
@@ -92,7 +92,7 @@
 		{#each quizzes as quiz (quiz.local_id)}
 			<tr class="hover:bg-gray-200 cursor-pointer">
 				<td class="border-b border-slate-100 dark:border-slate-700 p-1">
-					<a href={`${base}/quizzes/{quiz.local_id}`}>{quiz.name}</a>
+					<a href={`${base}/quizzes/${quiz.local_id}`}>{quiz.name}</a>
 				</td>
 				<td class="border-b border-slate-100 dark:border-slate-700 p-1"
 					>{quiz.created_at.toLocaleString()}</td
